a4d32cdfb0f612766271dae9f44e2ed50f4367d4
Mario93Mechelen <r0334050@student.thomasmore.be>
2017-03-03 17:31:27 +0100

    modified the details page

--- php/lesweek 2/shirt store/details.php
+++ php/lesweek 2/shirt store/details.php
@@ -12,11 +12,35 @@ $productId = $_GET['productid'];
 		{
 			width:50px;
 		}
+		a,p,img
+		{
+			float:left;
+		}
+		p
+		{
+			margin-top:80px;
+		}
+		#key
+		{
+			display:none;
+		}
+		button
+		{
+			float:left;
+			margin-top:80px;
+		}
 	</style>
 </head>
 <body>
+	<div>
 	<a href="products.php">Back to store</a>
 	<p><?php echo $products[$productId]['title']; ?></p>
 	<img src="<?php echo $products[$productId]['pic']; ?>" alt="tie">
+	<form action="" method="post">
+		<label for="key"></label>
+		<input type="text" id="key" name="key" value="<?php echo $productId; ?>">
+		<button>Buy Now</button>
+	</form>
+	</div>
 </body>
 </html>
