8469b30b37813e9ffb8f5d4e472c35a7170847f9
Mario93Mechelen <r0334050@student.thomasmore.be>
2017-03-03 15:17:51 +0100

    added the products array

---
+++ php/lesweek 2/shirt store/products.inc.php
@@ -0,0 +1,19 @@
+<?php
+$products = [
+	[
+	"title"=>"blue tie €99.99",
+	"pic"=>"http://c.shld.net/rpx/i/s/i/spin/image/spin_prod_401486601??hei=64&wid=64&qlt=50",
+	"info"=>"more info..."
+	],
+	[
+	"title"=>"brown tie €49.99",
+	"pic"=>"https://www.drakes.com/media/catalog/product/cache/1/image/698x800/9df78eab33525d08d6e5fb8d27136e95/n/s/nseh.06449.013_a2_2/Drake-s-Brown-Handrolled-Silk-Tussah-Tie-NSEH.06449.013-35.jpg",
+	"info"=>"more info..."
+	],
+	[
+	"title"=>"red tie €69.99",
+	"pic"=>"http://beertees.com/media/catalog/product/cache/8/image/650x/040ec09b1e35df139433887a97daa66f/m/s/msc-0518.jpg",
+	"info"=>"more info..."
+	],
+]
+?>
