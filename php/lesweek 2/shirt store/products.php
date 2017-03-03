fd9543e30c77c16f24bc103b5f45437682eedeb3
Mario93Mechelen <r0334050@student.thomasmore.be>
2017-03-03 15:30:43 +0100

    finished the products page

--- php/lesweek 2/shirt store/products.php
+++ php/lesweek 2/shirt store/products.php
@@ -3,8 +3,24 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Products</title>
+	<style>
+		img
+		{
+			width:50px;
+		}
+	</style>
 </head>
 <body>
-	
+<?php include_once 'products.inc.php'; ?>
+	<ul>
+		<?php foreach($products as $p): ?>
+		<li>
+			<p><?php echo $p['title']; ?>
+			</p>
+			<img src="<?php echo $p['pic']; ?>" alt="tie">
+			<a href=""><?php echo $p['info']; ?></a>
+		</li>
+		<?php endforeach; ?>
+	</ul>
 </body>
 </html>
