12974a2a5ad304456c4ebc6083c684e3c8f0f0d6
Mario93Mechelen <r0334050@student.thomasmore.be>
2017-03-09 20:46:00 +0100

    added a tracks page with php functionality

---
+++ php/lesweek 3/spotify/tracks.php
@@ -0,0 +1,82 @@
+<?php
+	session_start();
+	$albumid=$_GET['albumid'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Tracks</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<style>
+		body
+		{
+			background-color: black;
+			font-family: montserrat;
+			color:white;
+		}
+		.container
+		{
+			width:80%;
+			margin:auto;
+			display:flex;
+			flex-direction: column;
+			flex-wrap: wrap;
+			justify-content:space-around;
+		}
+		.artists
+		{
+			background-color: darkslategrey;
+			height:150px;
+			width:100%;
+			margin:50px;
+			display:flex;
+			flex-direction: row;
+			cursor:pointer;
+		}
+		a
+		{
+			text-align: center;
+			text-decoration: none;
+			color:white;
+			font-family:montserrat
+		}
+		.photo
+		{
+			background-color: white;
+			order:-1;
+			width:150px;
+			height:150px;
+			margin-right:50px;
+		}
+		.logout
+		{
+			background: transparent;
+			border:2px solid white;
+			width:100px;
+			height:50px;
+			border-radius:50px;
+			float:right;
+			margin-right:100px;
+		}
+	</style>
+</head>
+<body>
+<a href="login.php?logout=true"><div class="logout"><p>Log Out</p></div></a>
+<div class="container">
+	<!--<div class="artists"><div class="photo"></div><p class="artisname"></p></div>-->
+	<?php
+		$con = new mysqli("localhost","root","","spotify");
+		$query = "SELECT * FROM albums WHERE (id = '".$albumid."');";
+		$result = $con->query($query);
+		$res = $result->fetch_assoc();
+		echo "<div class='artists'><img class='photo' src='".$res['cover']."' alt='cover'><p class='artisname'>".$res['title']."</p></div>";
+		$query2 = "SELECT * FROM tracks where(album_id='".$albumid."');";
+		$result2 = $con->query($query2);
+		while( $row = mysqli_fetch_array($result2) ){
+			echo"<p>".$row['title']."</p>";
+		}
+	?>	
+	</div>
+</body>
+</html>
