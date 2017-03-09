1c4f48740aacc2c37a4d68813c69cd50fbe354b3
Mario93Mechelen <r0334050@student.thomasmore.be>
2017-03-09 14:53:01 +0100

    added folders for spotify and lesweek 3 and added html structure

---
+++ php/lesweek 3/spotify/register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Register</title>
+</head>
+<body>
+	<div class="container">
+		<div class="form">
+			<div class="logosp"></div>
+			<div class="loginfb">
+				<a href="">
+					LOG IN WITH FACEBOOK
+				</a>
+			</div>
+			<form action="" method="post">
+				<label for="username">Username</label>
+				<input type="text" name="username" id="username">
+				<label for="password">Password</label>
+				<input type="text" name="password" id="password">
+				<div class="robot">
+				<input type="checkbox" name="robot" id="robot" value="I'm not a robot">
+				<p>I'm not a robot</p>
+				</div>
+				<button>
+					LOGIN
+				</button>
+			</form>
+		</div>
+		<div class="title">
+			<H1>Get the right music right now</H1>
+		</div>
+	</div>
+</body>
+</html>
